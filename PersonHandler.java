2ac885271e31dddc14b651de09f79c85e861912c
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro.local>
2019-02-08 19:55:52 -0500

    added person method loos

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,4 +1,4 @@
-package com.zipcodewilmington;
+ 
 
 /**
  * Created by leon on 1/24/18.
@@ -12,14 +12,24 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
+        int len = personArray.length;
+        int i=0;
+        
+        System.out.println(len);
+        
         // assume there is a `counter`
         // while `counter` is less than length of array
             // begin loop
+            while(i<len){
+                result = result + personArray[i].toString();
+                i++;
+                
+            
 
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
-
+            }
             // end loop
         return result;
     }
@@ -34,10 +44,14 @@ public class PersonHandler {
 
         // use the above clauses to declare for-loop signature
             // begin loop
+            for(int i=0;i<personArray.length;i++){
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
+                result = result + personArray[i].toString();
             // end loop
+            
+        }
 
         return result;
     }
@@ -50,11 +64,13 @@ public class PersonHandler {
         // identify array's variable-name
 
         // use the above discoveries to declare for-each-loop signature
+        for(Person ele: personArray) {
             // begin loop
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
-
+              result = result + ele.toString();
+        }
         return result;
     }
 
